2910fb78276ccf20fdd6d07cceba98e341a44f16
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-18 00:30:21 +0100

    project Architecture

---
+++ BookManagement.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookManagement.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ BookManagement.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookManagement.Service
+{
+    public class Class1
+    {
+    }
+}
