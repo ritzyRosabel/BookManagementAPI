52454e87298960e91c8906a922643a279c3952ac
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-18 11:22:11 +0100

    implementation for Ibookservice

--- BookManagement.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BookManagement.Domain
-{
-    public class Class1
-    {
-    }
-}
--- BookManagement.Service/Implementation/BookService.cs
+++ BookManagement.Service/Implementation/BookService.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using BookManagement.Core.Interface;
+using BookManagement.Domain.API;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookManagement.Service.Implementation
 {
-    class BookService
+    public class BookService : IBookService
     {
+        public Response CreateBooks(BookRequest book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response DeleteABook(int bookID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response UpdateABook(int bookID)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
