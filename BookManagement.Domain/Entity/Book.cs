8d449f7eeecf4019e9098f46cb0f81d1f997498e
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-18 10:22:16 +0100

    book class, bookcontroller, bookservice

--- BookManagement.Domain/Entity/Book.cs
+++ BookManagement.Domain/Entity/Book.cs
@@ -9,6 +9,6 @@ namespace BookManagement.Domain.Entity
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
-        public string ISBN { get; set; }
+        public string ISBN  { get; set; }
     }
 }
