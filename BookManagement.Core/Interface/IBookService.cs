2ef3d724a72302c69ccbd294c7e62247e08586b9
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-18 11:07:08 +0100

    Response class for te api endpoint

--- BookManagement.Core/Interface/IBookService.cs
+++ BookManagement.Core/Interface/IBookService.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using BookManagement.Domain.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookManagement.Core.Interface
 {
-    class IBookService
+    public interface IBookService
     {
+     
     }
 }
---
+++ BookManagement.Domain/API/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookManagement.Domain.API
+{
+    public class Response
+    {
+        public string RequestId { get; set; }
+        public string RequestCode { get; set; }
+        public string RequestMessage { get; set; }
+        public Object Data { get; set; }
+    }
+}
