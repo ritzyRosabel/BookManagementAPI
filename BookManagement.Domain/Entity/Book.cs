2656252f3b3bda4b02bdb7313c1c301625045666
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-18 10:28:00 +0100

    primarykey for book

--- BookManagement.Domain/Entity/Book.cs
+++ BookManagement.Domain/Entity/Book.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookManagement.Domain.Entity
 {
    public class Book
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
