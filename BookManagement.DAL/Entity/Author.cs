52bbefdc8639cb21850b52444a77795239aebed1
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-21 02:39:22 +0100

    author table

--- BookManagement.DAL/Entity/Author.cs
+++ BookManagement.DAL/Entity/Author.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookManagement.DAL.Entity
 {
     public class Author
     {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDate { get; set; }    `
     }
 }
