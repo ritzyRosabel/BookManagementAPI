ee2adb08a25d9c2b132591a057453156bac49631
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-21 02:16:41 +0100

    service extensions

---
+++ BookManagement.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManagement.API.Extensions
+{
+    public class ServiceExtensions
+    {
+    }
+}
