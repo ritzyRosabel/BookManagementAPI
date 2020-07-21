c2987ff8752217d58b86c9bcd1e2fc56bdf38ecf
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-21 02:52:14 +0100

    implementing the IEntity Interface

--- BookManagement.DAL/Entity/BaseEntity.cs
+++ BookManagement.DAL/Entity/BaseEntity.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using BookManagement.Core.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookManagement.DAL.Entity
 {
-    public class BaseEntity
+    public class BaseEntity : IEntity
     {
+        public int Id { get; set; }
     }
 }
