67388f7677b8ab46893a19b444cb6d3a0cf61cf5
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-28 23:14:37 +0100

    made some changes to repository Query interface

--- BookManagement.DAL/DataAccess/IRepositoryQuery.cs
+++ BookManagement.DAL/DataAccess/IRepositoryQuery.cs
@@ -6,11 +6,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using BookManagement.Core.Interface;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
 namespace BookManagement.DAL.DataAccess
 {
     public interface IRepositoryQuery<TEntity,TPrimaryKey> where TEntity : class, IEntity<TPrimaryKey>
     {
-        Task<TEntity> Get();
-        Task<TEntity> GetById(TPrimaryKey  id);
+        Task<EntityEntry<TEntity>> Get();
+        Task<EntityEntry<TEntity>> GetById(TPrimaryKey  id);
     }
 }
