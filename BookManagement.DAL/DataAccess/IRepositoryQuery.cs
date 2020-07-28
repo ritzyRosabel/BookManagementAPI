5dfc4a63035dcf49a3551ba3695f096519bc5a06
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-28 23:30:37 +0100

    Repository Query class Update

--- BookManagement.DAL/DataAccess/IRepositoryQuery.cs
+++ BookManagement.DAL/DataAccess/IRepositoryQuery.cs
@@ -5,14 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using BookManagement.Core.Interface;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace BookManagement.DAL.DataAccess
 {
     public interface IRepositoryQuery<TEntity,TPrimaryKey> where TEntity : class, IEntity<TPrimaryKey>
     {
-        Task<EntityEntry<TEntity>> Get();
-        Task<EntityEntry<TEntity>> GetById(TPrimaryKey  id);
+        Task<List<TEntity>> Get();
+        Task<TEntity> GetById(TPrimaryKey  id);
     }
 }
--- BookManagement.DAL/DataAccess/RepositoryQuery.cs
+++ BookManagement.DAL/DataAccess/RepositoryQuery.cs
@@ -1,36 +1,39 @@
 ﻿
 using BookManagement.Core.API;
+using BookManagement.Core.Interface;
 using BookManagement.Core.Utility;
 using BookManagement.DAL.DataAccess;
 using BookManagement.DAL.DB;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookManagement.Domain.DBRepository
 {
-    public class RepositoryQuery<IEntity> : IRepositoryQuery<Response>
+    public class RepositoryQuery<TEntity, TPrimaryKey> : IRepositoryQuery<TEntity,TPrimaryKey> where TEntity:class,IEntity<TPrimaryKey>
     {
         private readonly ApplicationDbContext _db;
+        public virtual DbSet<TEntity> entities =>_db.Set<TEntity>();
         public RepositoryQuery(ApplicationDbContext db)
         {
             _db = db;
         }
-        public async Task<Response> Get()
+        public async Task<List<TEntity>> Get()
         {
-            var response = Utilities.InitializeResponse();
-            response.Data =  await _db.Books.ToListAsync();
-            return response;
+            var result = await entities.ToListAsync();
+            return result;
+
         }
 
-        public async Task<Response> GetById(int id)
+        public async Task<TEntity> GetById(TPrimaryKey id)
         {
-            var response = Utilities.InitializeResponse();
-            response.Data =  await _db.Books.FirstOrDefaultAsync(B=>B.Id==id);
-            return response;
+            var result =await entities.FindAsync(id);
+            return result;
         
         }
     }
