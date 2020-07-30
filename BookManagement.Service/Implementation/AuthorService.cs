9c7ca502d504c9be72ef6cb7cd502ca4f42ee4b4
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-30 01:18:48 +0100

    Get method for Author Service

--- BookManagement.Service/Implementation/AuthorService.cs
+++ BookManagement.Service/Implementation/AuthorService.cs
@@ -1,20 +1,32 @@
-﻿using BookManagement.Core.Interface;
+﻿using BookManagement.Core.API;
+using BookManagement.Core.Interface;
 using BookManagement.DAL.DataAccess;
 using BookManagement.DAL.Entity;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BookManagement.Service.Implementation
 {
     public class AuthorService
     {
-        private readonly IRepositoryCommand<Author,int> _repositoryCommand;
-        private readonly IRepositoryQuery<Author,int> _repositoyQuery;
-        public AuthorService(IRepositoryCommand<Author,int>repositoryCommand,IRepositoryQuery<Author,int> repositoryQuery)
+        private readonly IRepositoryCommand<Author, int> _repositoryCommand;
+        private readonly IRepositoryQuery<Author, int> _repositoyQuery;
+        public AuthorService(IRepositoryCommand<Author, int> repositoryCommand, IRepositoryQuery<Author, int> repositoryQuery)
         {
             _repositoryCommand = repositoryCommand;
             _repositoyQuery = repositoryQuery;
         }
+
+        public async Task<Response> Get()
+        {
+            var response = new Response();
+            var result = await _repositoyQuery.Get();
+            response.Data = result;
+            return response; 
+        }
+
+        
     }
 }
