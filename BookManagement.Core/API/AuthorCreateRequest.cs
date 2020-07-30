c60d3c151d0ab67d788ae9a6faa60b405f5cfa76
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-30 01:28:38 +0100

    AuthorCreateRequest

---
+++ BookManagement.Core/API/AuthorCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookManagement.Core.API
+{
+    public class AuthorCreateRequest
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
