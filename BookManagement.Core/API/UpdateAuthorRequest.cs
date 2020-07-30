b7406068c879871699188d5836f55d840cfcecd6
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-30 01:39:29 +0100

    UpdateAuthorRequest

---
+++ BookManagement.Core/API/UpdateAuthorRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookManagement.Core.API
+{
+    public class UpdateAuthorRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
